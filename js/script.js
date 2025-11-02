5d8b7b31ec3eb2216934971a4833bc553f6f8577
Hande ÖZ <handeeeozzzz@gmail.com>
2025-11-02 13:39:39 +0300

    tahterevalliyetıklamayapıncarastgelesayıuretildi

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ const result = document.getElementById("result");
 
 ctx.fillStyle = "black";
 ctx.fillRect(50, 100, 400, 40);
+
+c.addEventListener("click", function () {
+  const ramdomNumber = Math.floor(Math.random() * 100);
+  result.innerText = ramdomNumber;
+});
