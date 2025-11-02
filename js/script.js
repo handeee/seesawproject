60ed06b5bf880dca9bec3a26770c870b7e4bc94d
Hande ÖZ <handeeeozzzz@gmail.com>
2025-11-02 14:45:37 +0300

    Tahterevalliyefare tıklamasınlaağırlıkeklemetamamlandi

--- js/script.js
+++ js/script.js
@@ -6,34 +6,44 @@ ctx.fillStyle = "black";
 // tahterevallinin dönme açısı
 
 ctx.fillRect(50, 100, 400, 40);
-c.addEventListener("click", function () {
+c.addEventListener("click", function (event) {
   const ramdomNumber = Math.floor(Math.random() * 11);
   result.innerText = ramdomNumber;
-  agirliklar.push(ramdomNumber);
+  const rect = c.getBoundingClientRect();
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+  //agirlik ve pzoisyon kaydetme
+  agirliklar.push({ deger: ramdomNumber + " " + "kg", x: mouseX, y: mouseY });
+
   ctx.clearRect(0, 0, c.width, c.height); // eski çizimi temizle
   // Dereceyi hesapla ve sınırla
   let derece = (ramdomNumber - 5) * 5;
   derece = Math.max(-30, Math.min(30, derece));
-  ctx.save(); // mevcut transformu kaydet
 
+  ctx.save(); // mevcut transformu kaydet
   ctx.translate(250, 120); // dikdörtgenin ortasına taşı
   ctx.rotate((derece * Math.PI) / 180); // rastgele açı
+
   ctx.fillStyle = "black";
   ctx.fillRect(-200, -20, 400, 40); // ortadan çiz
 
-  agirliklar.forEach((item, index) => {
-    ctx.beginPath();
+  agirliklar.forEach((item) => {
+    const x = item.x;
+    const y = item.y;
+    //daire çizimi
     ctx.fillStyle = "pink";
-    //daireleri tahtaya eşit aralıklarda koymak
-    const x = -180 + index * 40;
-    const y = -10;
-    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(item.x - 250, item.y - 120, 15, 0, 2 * Math.PI);
     ctx.fill();
-    //ağğırlık numarasını daire içine yazmak
+    ctx.stroke();
+    //ağırlık numarasını daire içine yazmak
     ctx.fillStyle = "black";
     ctx.textAlign = "center";
-    ctx.font = "10px Arial";
-    ctx.fillText(item, x, y);
+    ctx.font = "14px Arial";
+    ctx.textBaseline = "middle";
+    ctx.fillText(item.deger, item.x - 250, item.y - 120);
   });
   ctx.restore();
 });
