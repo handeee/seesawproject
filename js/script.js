3af9f15b11d9c10c2e3ae35299de28a8470afb1e
Hande ÖZ <handeeeozzzz@gmail.com>
2025-11-02 13:56:37 +0300

    randomsayıagirliginagoreacidegisimi

--- js/script.js
+++ js/script.js
@@ -4,9 +4,19 @@ const result = document.getElementById("result");
 
 ctx.fillStyle = "black";
 // tahterevallinin dönme açısı
-ctx.rotate((20 * Math.PI) / 180);
+
 ctx.fillRect(50, 100, 400, 40);
 c.addEventListener("click", function () {
   const ramdomNumber = Math.floor(Math.random() * 100);
   result.innerText = ramdomNumber;
+
+  ctx.clearRect(0, 0, c.width, c.height); // eski çizimi temizle
+  ctx.save(); // mevcut transformu kaydet
+
+  ctx.translate(350, 120); // dikdörtgenin ortasına taşı
+  ctx.rotate(((ramdomNumber - 50) * Math.PI) / 180); // rastgele açı
+  ctx.fillStyle = "black";
+  ctx.fillRect(-200, -20, 400, 40); // ortadan çiz
+
+  ctx.restore(); // transformu eski haline getir
 });
