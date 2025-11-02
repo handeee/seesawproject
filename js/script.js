78553329bf3f1acc5238c656c1632e8af74a0e37
Hande ÖZ <handeeeozzzz@gmail.com>
2025-11-02 16:26:17 +0300

    cssduzenlendi

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
 const result = document.getElementById("result");
-let agirliklar = [];
-ctx.fillStyle = "black";
-// tahterevallinin dönme açısı
+const resetBtn = document.getElementById("resetBtn");
+const aci = document.getElementById("aci");
 
+let agirliklar = [];
+
+ctx.fillStyle = "black";
+
+// tahterevallinin dönme açısı
 ctx.fillRect(50, 100, 400, 40);
+// reset butonuna tıklanınca
+// resetBtn.addEventListener("click", function (event) {
+//   agirliklar = [];
+//   ctx.clearRect(0, 0, c.width, c.height);
+//   ctx.fillStyle = "black";
+//   ctx.fillRect(50, 100, 400, 40);
+//   result.innerText = "";
+//   aci.innerText = "0°";
+// });
 c.addEventListener("click", function (event) {
+  // Eğer tıklanan yer butonsa, işlem yapma
+  if (event.target.id === "resetBtn") return;
+
   const ramdomNumber = Math.floor(Math.random() * 11);
   result.innerText = ramdomNumber;
   const rect = c.getBoundingClientRect();
@@ -19,7 +35,7 @@ c.addEventListener("click", function (event) {
   // Dereceyi hesapla ve sınırla
   let derece = (ramdomNumber - 5) * 5;
   derece = Math.max(-30, Math.min(30, derece));
-
+  aci.innerText = derece + "°";
   ctx.save(); // mevcut transformu kaydet
   ctx.translate(250, 120); // dikdörtgenin ortasına taşı
   ctx.rotate((derece * Math.PI) / 180); // rastgele açı
