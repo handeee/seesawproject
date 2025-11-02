2be8d0e2216c40febe48fae2ec117fa3231990c3
Hande ÖZ <handeeeozzzz@gmail.com>
2025-11-02 13:48:19 +0300

    tahterevallidonmeacisiuygulandı

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const ctx = c.getContext("2d");
 const result = document.getElementById("result");
 
 ctx.fillStyle = "black";
+// tahterevallinin dönme açısı
+ctx.rotate((20 * Math.PI) / 180);
 ctx.fillRect(50, 100, 400, 40);
-
 c.addEventListener("click", function () {
   const ramdomNumber = Math.floor(Math.random() * 100);
   result.innerText = ramdomNumber;
