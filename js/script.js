c2f9bf4f87883bd89ac2d7c8d9a146d6415fa03b
Hande ÖZ <handeeeozzzz@gmail.com>
2025-11-02 13:37:19 +0300

    fillrectkullanılaraktahterevallicizildi

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+const result = document.getElementById("result");
+
+ctx.fillStyle = "black";
+ctx.fillRect(50, 100, 400, 40);
